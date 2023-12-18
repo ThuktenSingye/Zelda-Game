b133ac7aebf589a6f7cff1f037095b5811ff9156
ThuktenSingye <thuktensingye2163@gmail.com>
2023-12-18 09:44:48 +0600

    #player inventory

--- Zelda-Game/inventory.c
+++ Zelda-Game/inventory.c
@@ -5,20 +5,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+    Allocates memory for the player's inventory, which is represented as a 2D array of characters.
+    Each row corresponds to an inventory slot, and each column stores the name of a treasure item. 
+    The inventory is initialized with empty strings for each slot.
+*/
 char** createInventory(){
     char** inventory;
 
-    // allocate for rows
+    /* Allocate memory for row*/
     inventory = (char**)malloc(sizeof(char*) * MAX_INVENTORY_SIZE);
-    // for column
+    
+    /* Allocate memory for column*/
     for(int i =0; i< MAX_INVENTORY_SIZE; i++){
         inventory[i] = (char*)malloc(sizeof(char) * MAX_TREASURE_NAME_LENGTH);
-        // initialized with emoty 
+        /* Initialized with null character*/ 
         inventory[i][0] = '\0';
     }
 
     return inventory;
 }
+
+
+/*
+    Adds a treasure item to the player's inventory. It searches for an empty slot in the inventory, 
+    copies the treasure name into that slot, and ensures null-termination to prevent memory issues.
+*/
 void addItemToInventory(char **inventory, char* treasure){
     // Find an empty slot in the inventory
     for (int i = 0; i < MAX_INVENTORY_SIZE; i++) {
@@ -33,12 +45,20 @@ void addItemToInventory(char **inventory, char* treasure){
     }
 }
 
+
+/* Displays the contents of the player's inventory on the terminal. Prints each item in a tab-separated format. */
+
 void displayInventoryList(char **inventory){
     for(int i=0; i< MAX_INVENTORY_SIZE; i++){
         printf("%s\t", inventory[i]);
     }
 }
 
+
+/*
+    Counts and returns the number of items in the player's inventory. 
+    Iterates through the inventory, checking the length of each item's name to determine if the slot is occupied.
+*/
 int getInventoryItemLength(char **inventory){
     int itemCount = 0;
     for(int i =0; i< MAX_INVENTORY_SIZE; i++){
@@ -49,6 +69,12 @@ int getInventoryItemLength(char **inventory){
     return itemCount;
 }
 
+
+/*
+    Frees the memory allocated for the player's inventory. Releases both the rows and columns of the 2D array,
+    ensuring that all memory used for storing treasure names is properly deallocated.
+
+*/
 void destroyInventory(char** inventory) {
     for (int i = 0; i < MAX_INVENTORY_SIZE; i++) {
         free(inventory[i]);
