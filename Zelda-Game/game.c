21c2cf72e25e81f172ba96041e1449990ad78007
ThuktenSingye <thuktensingye2163@gmail.com>
2023-12-18 09:44:23 +0600

    #main logic of the game

--- Zelda-Game/game.c
+++ Zelda-Game/game.c
@@ -2,50 +2,76 @@
 #include "player.h"
 #include "mapmaker.h"
 #include "terminal.h"
+#include "inventory.h"
+#include "ucpSleep.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-void startGame() {
-    // Your game logic goes here
-    printf("The game has started!\n");
-    // first setup the game like setting creating and displaying map
+/*
+    This function serves as the entry point for running the game. It calls 
+    the setUpGame() function to initialize and start the game.
+*/
+void runGame() 
+{
+    /* First setup the game like setting, creating, and displaying the map */
     setUpGame();
-
+    
 }
 
-void setUpGame(){
-    printf("Setting up the game\n");
+
+/*  
+    This function sets up the game environment by obtaining map metadata, creating the game map 
+    and inventory, parsing the initial player position and type, initializing the player, and 
+    starting the game with the provided settings.
+
+*/
+void setUpGame()
+{
     int itemCount, mapRowSize, mapColSize;
 
-    // setting up the player
-    Player mainPlayer;
-    initializedPlayer(&mainPlayer, 0, 0);
-    displayPlayerPosition(&mainPlayer);
-    // get map meta data 
+    /* Get map metadata and initialize variables */
     char** mapMetaData = getItemData(&itemCount, &mapRowSize, &mapColSize);
     
-    printf("%s\n",mapMetaData[0]);
+    /* Create the game map and inventory */
+    char** map = createMap(mapMetaData, mapRowSize, mapColSize, itemCount);
+    char** inventory = createInventory();
 
-    // outputting the meta data
-    // for (int i = 0; i < itemCount; i++) {
-    //     printf("%s\n", mapMetaData[i]);
-    // }
-
-    // creating the map
-    char** map = createMap(mapMetaData,mapRowSize, mapColSize, itemCount);
-    // display the map
-    // displayMap(map, mapRowSize, mapColSize);
-    runGame(map, mapRowSize, mapColSize);
-
+    /* Parse initial player position and type */
+    int initialX, initialY;
+    char type, description[MAX_STRING_LENGTH];
+    sscanf(mapMetaData[0], "%d,%d %c %[^\n]", &initialX, &initialY, &type, description);
     
+    /* Initialize the player */
+    Player mainPlayer;
+    initializedPlayer(&mainPlayer, initialX, initialY);
+    
+    /* Initialize map information */
+    MapInfo mapInfo = 
+    {
+        .map = map,
+        .mapMetaData = mapMetaData,
+        .mapRowSize = mapRowSize,
+        .mapColSize = mapColSize,
+        .itemCount = itemCount
+    };
 
+    /* Start the game with initialized settings */
+    startGame(&mapInfo, &mainPlayer, inventory);
 
-};
+    /* Free allocated memory for map metadata */
+    freeMapItemData(mapMetaData, itemCount);
+}
 
-void displayMap(char** map, int mapRowSize, int mapColSize){
-    // display map
-    printf("This is the world map:\n");
+/*
+    Displays the game map on the terminal, showing the layout of the current game world. 
+    Empty squares are represented by periods ('.'), and other characters represent different 
+    elements in the game.
+*/
+void displayMap(char** map, int mapRowSize, int mapColSize)
+{
+    /* Display the map */
     for (int i = 0; i < mapRowSize; i++) {
         for (int j = 0; j < mapColSize; j++) {
             if (map[i][j] == '\0') {
@@ -56,52 +82,294 @@ void displayMap(char** map, int mapRowSize, int mapColSize){
         }
         printf("\n");
     }
-
 }
 
-char** createMap(char** mapMetaData, int mapRowSize, int mapColSize, int itemCount){
-    // this function will be used to create dynamic 2D map
-    char** map = (char**)malloc(sizeof(char*) * mapRowSize); // allocating for rows
-
-    // allocating memory for each rows
-    for(int i=0; i< mapRowSize; i++){
+/*
+    Allocates memory for the game map, reads item data from map metadata, and assigns items to 
+    their corresponding positions on the map based on x and y coordinates.
+*/
+char** createMap(char** mapMetaData, int mapRowSize, int mapColSize, int itemCount)
+{
+    /* Allocate memory for the map */
+    char** map = (char**)malloc(sizeof(char*) * mapRowSize);
+    
+    /* Allocate memory for each row */
+    for(int i = 0; i < mapRowSize; i++){
         map[i] = (char*)malloc(sizeof(char) * mapColSize);
-    };
+    }
 
-    // parse and allocate item to the map
-    for(int i =0; i< itemCount; i++){
+    /* Parse and allocate items to the map */
+    for(int i = 0; i < itemCount; i++){
         int x, y;
         char type, description[MAX_STRING_LENGTH];
-        // reading x, y, type and description
+        /* Read x, y, type, and description */
         sscanf(mapMetaData[i], "%d,%d %c %[^\n]", &x, &y, &type, description);
-        // allocating type to map based on its (x, y)
+        /* Allocate type to map based on its (x, y) */
         map[x][y] = type;
     }
-
-
+ 
     return map;
-};
+}
 
-void runGame(char** map, int mapRowSize, int mapColSize){
-    // this is where main game logic lies
+/*
+    Initiates the main game loop, processing player input and updating the game state accordingly.
+    Manages player movement, interactions with map elements, inventory handling, and the overall flow of the game.
+*/
+void startGame(MapInfo *mapInfo, Player* player, char** inventory) 
+{
+    /* Game variables and flags */
     bool gameEnded = false;
     bool hasSword = false;
+    bool foundDummy = false;
+    int dummyLife = 10;
+    char currentPlayerDirection = '>';
+    int newPosX = player->x;
+    int newPosY = player->y;
 
-    // disabling buffering and echoing
+    /* Display the initial world map */
+    printf("This is the world map:\n");
+    displayMap(mapInfo->map, mapInfo->mapRowSize, mapInfo->mapColSize);
+
+    /* Disable terminal buffering and echoing */
     disableBuffer();
-    while(!gameEnded){
-        printf("Game running\n");
-        displayMap(map, mapRowSize, mapColSize);
-        printf("\n\nWhere do you want to go?\nW, A, S, D, (Q)uit, (I)nventory\n");
-        // get an input from user using terminal
+    
+    // Main game loop
+    while (!gameEnded) 
+    {
+        /* Display movement options to the player */
+        printf("\n\nWhere do you want to go?\nW, A, S, D, (Q)uit, (I)nventory\n\n");
+
+        /* Get input from the user using terminal */
         char input;
-        scanf("%c", &input);
-        printf("The user has entered %c\n", input);
+        if (scanf("%c", &input) != 1) 
+        {
+            fprintf(stderr, "Error reading input.\n");
+            exit(EXIT_FAILURE);
+        }
+        clearScreen();
 
-         
+        /* Process player input */
+        switch (input) {
+            /* Cases for different player actions */
+            case 'w':
+                /* Move the player north */
+                printf("Going North\n");
+                newPosX--;
+                currentPlayerDirection = '^';
+                break;
+            case 's':
+                /* Move the player south */
+                printf("Going South\n");
+                newPosX++;
+                currentPlayerDirection = 'v';
+                break;
+            case 'a':
+                /* Move the player west */
+                printf("Going West\n");
+                newPosY--;
+                currentPlayerDirection = '<';
+                break;
+            case 'd':
+                /* Move the player east */
+                printf("Going East\n");
+                newPosY++;
+                currentPlayerDirection = '>';
+                break;
+            case 'i':
+                /* Display the inventory list */
+                displayInventoryList(inventory);
+                break;
+            case ' ':
+                /* Perform an action when the space key is pressed */
+                if (hasSword) {
+                    if (!foundDummy) 
+                    {
+                        /* Attack the square infront */
+                        attackSquare(currentPlayerDirection);
+                    } 
+                    else 
+                    {
+                        /* Attack the training dummies */
+                        gameEnded = destroyDummy(&dummyLife);
+                        if (gameEnded) {
+                            printf("The Training Dummies have been destroyed!\nYou Win!\n");
+                        }
+                    }
+                }
+                break;
+            case 'q':
+                /* Quit the game */
+                printf("Quitting the game\n");
+                gameEnded = true;
+                break;
+            default:
+                /* Ignore other keys; nothing should change in the world map */
+                printf("Ignoring other key, nothing should change in the world map\n");
+                break;
+        }
 
 
-        gameEnded = true;
+        /* Check if the new position is within boundaries to avoid segmentation fault */
+        if (newPosX >= 0 && newPosX < mapInfo->mapRowSize && newPosY >= 0 && newPosY < mapInfo->mapColSize) 
+        {
+            /* Check map elements and update game state */
+            switch (mapInfo->map[newPosX][newPosY]) 
+            {
+                case '\0':
+                    /* Empty square, move the player to the new position */
+                    movePlayer(player, mapInfo->map, newPosX, newPosY, currentPlayerDirection);
+                    foundDummy = false;
+                    break;
+                case '#':
+                    /* Wall square, stay in the current position */
+                    newPosX = player->x;
+                    newPosY = player->y;
+                    mapInfo->map[newPosX][newPosY] = currentPlayerDirection;
+                    foundDummy = false;
+                    break;
+                case 'T':
+                    /* Treasure square, move the player, collect treasure, and update inventory */
+                    movePlayer(player, mapInfo->map, newPosX, newPosY, currentPlayerDirection);
+                    /* Collect treasure description */
+                    char* treasure = getObjectDescription(newPosX, newPosY);
+                    printf("You found a Treasure! %s\n", treasure);
+                    foundDummy = false;
+                    collectTreasure(inventory, treasure);
+                    /* Free treasure memory */
+                    free(treasure);
+                    break;
+                case 'O':
+                    /* Old man square, provide information and give sword if the player doesn't have one */
+                    if (!hasSword) 
+                    {
+                        /* Get the object description */
+                        char* objectDescription = getObjectDescription(newPosX, newPosY);
+                        if (objectDescription != NULL) 
+                        {
+                            printf("You see an old man: He says: %s\n", objectDescription);
+                            /* Give sword to the player */
+                            hasSword = true;
+                            /* Free the object description memory */
+                            free(objectDescription);
+                        } 
+                        else 
+                        {
+                            /* Handle error or memory allocation failure when getting object description */
+                            printf("Error: Failed to get object description.\n");
+                        }
+                    }
+                    /* Set the player's position to the initial position */
+                    newPosX = player->x;
+                    newPosY = player->y;
+                    mapInfo->map[newPosX][newPosY] = currentPlayerDirection;
+                    foundDummy = false;
+                    break;
+                case 'D':
+                    /* Dummy square, provide information and set foundDummy to true */
+                    char* dummyDescription = getObjectDescription(newPosX, newPosY);
+                    printf("You see %s\n", dummyDescription);
+                    foundDummy = true;
+                    /* Keep the player at the initial position */
+                    newPosX = player->x;
+                    newPosY = player->y;
+                    mapInfo->map[newPosX][newPosY] = currentPlayerDirection;
+                    free(dummyDescription);
+                    break;
+                default:
+                    /* Default case, no action needed */
+                    break;
+            }
+
+        } else 
+        {
+            /* Handle reaching the edge or do nothing */
+            newPosX = player->x;
+            newPosY = player->y;
+            mapInfo->map[newPosX][newPosY] = currentPlayerDirection;
+        }
+
+        /* Display the updated world map */
+        printf("\nThis is the world map:\n");
+        displayMap(mapInfo->map, mapInfo->mapRowSize, mapInfo->mapColSize);
     }
+
+    /* Free allocated memory and enable terminal buffering */
+    freeMap(mapInfo->map, mapInfo->mapRowSize);
+    destroyInventory(inventory);  
     enableBuffer();
+}
+
+/*
+    Simulates the destruction of a training dummy during combat. Reduces the dummy's hitpoints 
+    and checks if the dummy is destroyed, signaling a win condition.
+*/
+bool destroyDummy(int *dummyLife)
+{
+    if (*dummyLife > 0)
+    {
+        (*dummyLife)--;
+        printf("You hit the Training Dummy!\nIt has %d hitpoints remaining\n", *dummyLife);
+        if(*dummyLife == 0)
+        {
+            /* Win game */
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
+/*
+    Simulates the player attacking a square in the game world. Depending on 
+    the player's direction, it displays a vertical or horizontal line to represent the attack.
+*/
+void attackSquare(char currentPlayerDirection) 
+{
+    if (currentPlayerDirection == '^' || currentPlayerDirection == 'v') 
+    {
+        ucpSleep(0.5);
+        printf("|\n");
+    } 
+    else 
+    {
+        ucpSleep(0.5);
+        printf("-\n");
+    }
+}
+
+/*
+    Frees the memory allocated for the game map. Releases both the rows and columns of the 2D map array.
+*/
+void freeMap(char** map, int mapRowSize)
+{
+    for (int i = 0; i < mapRowSize; i++) 
+    {
+        free(map[i]);
+    }
+    free(map);
+}
+
+/*
+    Frees the memory allocated for map item data obtained from map metadata. Releases the memory for 
+    each string in the array of strings and the array itself.
+*/
+void freeMapItemData(char** table, int itemCount) 
+{
+    for (int i = 0; i < itemCount; i++) 
+    {
+        free(table[i]);  /* Free each string in the table */
+    }
+    free(table);  /* Free the array of strings */
+}
+
+/*
+    Clears the terminal screen, providing a cleaner interface for displaying the game.
+    Uses the system command "clear" to clear the screen, and reports an error if the command fails.
+*/
+void clearScreen() 
+{
+    if (system("clear") != 0) 
+    {
+        printf("Error clearing the screen.\n");
+    }
 }
--- Zelda-Game/game.h
+++ Zelda-Game/game.h
@@ -1,9 +1,22 @@
 #ifndef GAME_H
 #define GAME_H
 
-void startGame();
+#include "player.h"
+#include "mapmaker.h"
+#include <stdbool.h>
+
+void runGame();
 void setUpGame();
-char** createMap(char** mapMetadata, int mapRowSize,int mapColSize, int itemCount);
-void displayMap(char** map, int mapRowSize, int mapColSize);
-void runGame(char** map,int mapRowSize, int mapColSize);
+char** createMap(char **mapMetadata, int mapRowSize,int mapColSize, int itemCount);
+void displayMap(char **map, int mapRowSize, int mapColSize);
+void mainGame(char **map, char **inventory,Player *player, char currentPlayerDirection);
+void startGame(MapInfo *mapInfo, Player *player,char **inventory);
+bool destroyDummy(int *dummyLife);
+void attackSquare(char currentPlayerDirection);
+void clearScreen();
+void freeMap(char** map, int mapRowSize);
+void freeMapItemData(char** mapMetaData,int itemCount);
+
+
+
 #endif
